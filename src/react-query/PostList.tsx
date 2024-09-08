a434a9f87f7978c36ad22508f8b37c8aeb58148e
luke <305867377@qq.com>
2024-09-08 19:52:03 +1000

    parameterized queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,30 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import usePosts from './hooks/usePosts';
 
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+const PostList = () => {    
+  const [userId, setUserId] = useState<number>();
+  const {data: posts, error, isLoading} = usePosts(userId)
 
-const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
+  if(isLoading) return <p>Loading...</p>;
 
   return (
+    <>
+    <select onChange={e => setUserId(parseInt(e.target.value))} 
+        value={userId}
+        className="form-select mb-3">
+      <option value="">user1</option>
+      <option value="1">user2</option>
+      <option value="2">user3</option>
+    </select>
     <ul className="list-group">
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <li key={post.id} className="list-group-item">
           {post.title}
         </li>
       ))}
     </ul>
+    </>
   );
 };
 
---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }
+  
+const usePosts = (userId: number | undefined) => {
+    const fetchPost = () =>  
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts',{
+            params: {
+              userId
+            }
+          })
+          .then((res) => res.data)
+          
+    
+      return useQuery<Post[], Error>({
+        queryKey: userId ? ['users', userId, 'posts'] : ['posts'], 
+        queryFn: fetchPost,
+        staleTime: 1*60*1000,
+      })
+        
+    }
+export default usePosts
