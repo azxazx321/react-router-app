83febc074c737f0cb0268dcb6b8e9f0b0f0cd7d7
luke <305867377@qq.com>
2024-09-19 14:58:57 +1000

    creating a hook to access context

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import { useReducer } from 'react';
 import './App.css';
-
-import TaskList from './state-management/TaskList';
 import tasksReducer from './state-management/reducers/tasksReducer';
 import TasksContext from './state-management/context/tasksContext';
 import NavBar from './state-management/NavBar';
-import authReducer from './state-management/reducers/authReducer';
-import AuthContext from './state-management/context/authContext';
 import LoginStatus from './state-management/LoginStatus';
 import AuthProvider from './state-management/AuthProvider';
 
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, [])
-  const [user, authDispatch] = useReducer(authReducer, '')
 
 
-  return  <AuthProvider>
-            <TasksContext.Provider value={{tasks, dispatch: tasksDispatch}}>
-              <LoginStatus/>
-              <NavBar/>
-            </ TasksContext.Provider>
-          </AuthProvider>
+  return <AuthProvider>
+    <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+      <LoginStatus />
+      <NavBar />
+    </ TasksContext.Provider>
+  </AuthProvider>
 }
 
 export default App;
