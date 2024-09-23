9a50d8bee263d43330a21299e57836c1f71ddccd
luke <305867377@qq.com>
2024-09-23 14:17:54 +1000

    passing data with route parameters

--- src/routing/UserListPage.tsx
+++ src/routing/UserListPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const UserListPage = () => {
   const users = [
     { id: 1, name: 'Mosh' },
@@ -8,6 +10,7 @@ const UserListPage = () => {
     <ul className="list-group">
       {users.map((user) => (
         <li className="list-group-item" key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
           <a href="#">{user.name}</a>
         </li>
       ))}
